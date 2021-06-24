c3a5f7a11d99366ee9791a786bcd2a40e44b47a7
Masaki_Nishihara <n.m.s.s.11301130@icloud.com>
2021-06-24 14:36:23 +0900

    remove comments and contacts migrations

--- db/schema.rb
+++ db/schema.rb
@@ -15,24 +15,6 @@ ActiveRecord::Schema.define(version: 2021_06_23_082714) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "comments", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "feed_id"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["feed_id"], name: "index_comments_on_feed_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "favorites", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "feed_id"
@@ -62,8 +44,6 @@ ActiveRecord::Schema.define(version: 2021_06_23_082714) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "comments", "feeds"
-  add_foreign_key "comments", "users"
   add_foreign_key "favorites", "feeds"
   add_foreign_key "favorites", "users"
   add_foreign_key "feeds", "users"
